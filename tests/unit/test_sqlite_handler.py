dd48574370fda118a5412aadcfaa798bfe20098a
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2024-02-06 02:50:21 -0600

    Add unit tests for SqliteHandler class

---
+++ tests/unit/test_sqlite_handler.py
@@ -0,0 +1,38 @@
+"""This is the unit tests for the sqlite_handler module."""
+import unittest
+from pathlib import Path
+
+from sqlite_logger.sqlite_handler import SqliteHandler
+
+
+class TestSqliteHandler(unittest.TestCase):
+    """Test the SqliteHandler class."""
+
+    def test___init__(self):
+        """Test the __init__ method."""
+        # Create a SqliteHandler object
+        database_filepath = Path("logging.db")
+        handler = SqliteHandler(database_filepath)
+
+        # Assert that the handler.database attribute is a path object
+        self.assertIsInstance(handler.database_file, Path)
+
+        # Assert that the handler.database_file attribute is ":memory:" when no
+        # database_file argument is provided
+        handler = SqliteHandler()
+        self.assertEqual(handler.database_file, ":memory:")
+
+    @unittest.skip("Not implemented")
+    def test_emit(self):
+        """Test the emit method."""
+        # pylint: disable=protected-access
+        # Create a SqliteHandler object
+        handler = SqliteHandler()
+
+        # Verify that the emit method raises a NotImplementedError
+        with self.assertRaises(NotImplementedError):
+            handler.emit(None)
+
+
+if __name__ == "__main__":
+    unittest.main()
